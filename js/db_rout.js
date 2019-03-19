925d74b7b484a78b2f31c5c831069716d795eebe
Ubuntu <ubuntu@ip-172-31-35-120.us-east-2.compute.internal>
2019-03-19 12:12:25 +0000

    js

---
+++ js/db_rout.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+
+mysql = require('mysql');
+
+var connection = mysql.createConnection(
+  {
+    host :'localhost',
+    user : 'ubuntu',
+    password : 'zpfpdlf213',
+    database : 'mydb'
+  }
+)
+
+connection.connect();
+
+function insert_sensor(device,unit,type,value,seq,ip){
+  obj = {};
+  obj.device = device;
+  obj.unit   = unit;
+  obj.type   = type;
+  obj.value  = value;
+  obj.seq    = seq;
+  obj.ip     = ip.replace(/^.*:/, '');
+
+  var query = connection.query('insert into sensors set ?',obj
+  ,function(err,rows,cols){
+    if(err) throw err;
+    console.log("database insertion ok = %j",obj);
+  });
+}
+
+app.get('/log', function(req,res){
+  r=req.query;
+  console.log("GET %j",r);
+
+  insert_sensor(r.device,r.unit,r.type,r.value,r.seq,r.ip);
+  res.end('OK:'+JSON.stringify(req.query));
+
+
+});
+
+var server = app.listen(8080, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('listening at http://%s:%s',host,port);
+});
---
+++ js/graph.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+mysql = require('mysql');
+
+var connection = mysql.createConnection(
+  {
+    host :'localhost',
+    user : 'ubuntu',
+    password : 'zpfpdlf213',
+    database : 'mydb'
+  }
+)
+
+connection.connect();
+
+function insert_sensor(device,unit,type,value,seq,ip){
+  obj = {};
+  obj.device = device;
+  obj.unit   = unit;
+  obj.type   = type;
+  obj.value  = value;
+  obj.seq    = seq;
+  obj.ip     = ip.replace(/^.*:/, '');
+
+  var query = connection.query('insert into sensors set ?',obj
+  ,function(err,rows,cols){
+    if(err) throw err;
+    console.log("database insertion ok = %j",obj);
+  });
+}
+
+app.get('/log', function(req,res){
+  r=req.query;
+  console.log("GET %j",r);
+
+  insert_sensor(r.device,r.unit,r.type,r.value,r.seq,r.ip);
+  res.end('OK:'+JSON.stringify(req.query));
+
+
+});
+
+app.get('/graph', function (req, res) {
+      console.log('got app.get(graph)');
+      var html = fs.readFile('./graph.html', function (err, html) {
+            html = " "+ html
+            console.log('read file');
+
+            var qstr = 'select * from sensors ';
+            connection.query(qstr, function(err, rows, cols) {
+                    if (err) throw err;
+
+                    var data = "";
+                    var comma = ",";
+                    for (var i=0; i< rows.length; i++) {
+                               r = rows[i];
+                               //data += comma +" ["+ r.id +",00,38),"+ r.value +"]";
+                               data += comma +" ['"+r.id +"',"+ r.value +"]";
+                               comma = ",";
+                            }
+                    var header = "['time','temperature']"
+                    //var header = "data.addColumn('date', 'Date/Time');"
+                    //header += "data.addColumn('number', 'Temp');"
+                    html = html.replace("<%HEADER%>", header);
+                    html = html.replace("<%DATA%>", data);
+
+                    res.writeHeader(200, {"Content-Type": "text/html"});
+                    res.write(html);
+                    res.end();
+                  });
+          });
+})
+
+
+var server = app.listen(8080, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('listening at http://%s:%s',host,port);
+});
