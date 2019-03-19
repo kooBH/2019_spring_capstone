ececb8b0ced4022d7de70cd17846ea7cd4eab616
KBH <k01032633280@gmail.com>
2019-03-19 18:52:13 +0900

    example craete_table

---
+++ create_table.sql
@@ -0,0 +1,21 @@
+create database mydb;
+use mydb;
+
+create table sensors (
+  /* id is intger and is not null
+  has automatically increasing value
+  and is primary key
+   */
+id int not null auto_increment primary key,
+
+seq int(10) unsigned,
+device decimal(4,0) unsigned,
+unit decimal(2,0) unsigned,
+type char(1),
+value decimal(10,4),
+ip char(15),
+time TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+
+CREATE USER 'me'@'localhost' IDENTIFIED BY 'mypassword';
+GRANT ALL PRIVILEGES ON mydb.* TO 'me'@'localhost';
+FLUSH PRIVILEGES;
